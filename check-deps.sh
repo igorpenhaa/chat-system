e99bb41e0440a2eca3e41b97767950f3470cec55
Bruno Ribeiro <bbrunoo@icloud.com>
2025-10-03 11:42:40 -0300

    fix rpm packs

--- check-deps.sh
+++ check-deps.sh
@@ -2,39 +2,47 @@
 
 HARDDEPS='bash awk getopt /usr/bin/time timeout make cmake bc'
 
-PACKAGES='libprotobuf-dev libgrpc-dev libgrpc++-dev'
+dpkg_PACKAGES='libprotobuf-dev libgrpc-dev libgrpc++-dev'
+rpm_PACKAGES='protobuf-devel grpc-devel'
 
 COMPILERS='gcc g++ python3 bash node'
 
 MANAGER=""
+PACKS=""
 if command -v dpkg &> /dev/null; then
 	MANAGER="dpkg-query -s"
+	PACKS=$dpkg_PACKAGES
 elif command -v rpm &> /dev/null; then
 	MANAGER="rpm -q --requires"
+	PACKS=$rpm_PACKAGES
 else
 	echo "Package manager is not dpkg or rpm."
-	return 1
+	exit 1
 fi
 
-err=0
+reterr=0
 function checkdeps() {
+	local err=0
 	for arg; do
 		if ! which $arg &> /dev/null; then
 			echo "$arg"
 			((err++))
 		fi
 	done
+	((reterr += err))
 	return $err
 
 }
 
 function checkpacks() {
+	local err=0
 	for arg; do
 		if ! $MANAGER "$arg" &> /dev/null; then
 			echo "$arg"
 			((err++))
 		fi
 	done
+	((reterr += err))
 	return $err
 }
 
@@ -47,4 +55,6 @@ checkdeps $COMPILERS && echo 'OK.'
 
 echo
 echo 'Checking Packages'
-checkpacks $PACKAGES && echo 'OK.'
+checkpacks $PACKS && echo 'OK.'
+
+exit $reterr
