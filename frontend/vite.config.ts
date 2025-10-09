801d0ce47ad9f7c093a0f4ccbe46f088398e9502
Igor Penha <igorpenha.it@gmail.com>
2025-10-08 21:59:50 -0300

    feat: add proxy to frontend, dont need anymore to know the ip of minikub

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,11 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//export default defineConfig({
+//  plugins: [react()],
+//  server: {
+//    host: '0.0.0.0',
+//    port: 3000,
+//  }
+//})
+
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
     port: 3000,
-  }
+    proxy: {
+      '/api': {
+        target: 'http://central.chat-system.svc.cluster.local:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+  },
 })
 
